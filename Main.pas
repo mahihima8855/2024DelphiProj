768a39e4a5bf2bf0a1a489949981cdd77d2d9c68
mahihima8855 <61259068+mahihima8855@users.noreply.github.com>
2024-02-04 23:39:47 +0900

    pre2

--- Main.pas
+++ Main.pas
@@ -8,12 +8,26 @@ uses
   uniGUIClasses, uniGUIRegClasses, uniGUIForm, FireDAC.Stan.Intf,
   FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
   FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
-  FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client, uniGUIBaseClasses, uniEdit;
+  FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client, uniGUIBaseClasses, uniEdit,
+  FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, uniPageControl,
+  uniStatusBar, uniSplitter, uniMemo, uniPanel, uniToolBar, FireDAC.Comp.DataSet;
 
 type
   TMainForm = class(TUniForm)
     FDConnection1: TFDConnection;
-    UniEdit1: TUniEdit;
+    FDMemTable1: TFDMemTable;
+    UniToolBar1: TUniToolBar;
+    UniPanel1: TUniPanel;
+    UniMemo1: TUniMemo;
+    UniSplitter1: TUniSplitter;
+    UniPanel2: TUniPanel;
+    UniSplitter2: TUniSplitter;
+    UniPanel3: TUniPanel;
+    UniStatusBar1: TUniStatusBar;
+    UniPageControl1: TUniPageControl;
+    UniTabSheet_main: TUniTabSheet;
+    UniStatusBar2: TUniStatusBar;
+    UniToolBar2: TUniToolBar;
   private
     { Private declarations }
   public
